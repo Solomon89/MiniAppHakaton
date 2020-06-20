a385bffcb5e7c7b397b8edf6d814def916c6846e
Дмитрий Владимирович Соломонов <solomon89@list.ru>
2020-06-21 00:09:27 +0300

    Add strava token

--- Controllers/StravaController.cs
+++ Controllers/StravaController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MiniAppHakaton.Core;
 using MiniAppHakaton.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MiniAppHakaton.Controllers
@@ -13,7 +19,6 @@ namespace MiniAppHakaton.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
-
         public StravaController(ApplicationDbContext context)
         {
             _context = context;
@@ -31,12 +36,46 @@ namespace MiniAppHakaton.Controllers
         public IActionResult AuthInStrava()
         {
 
-            return new RedirectResult($"http://www.strava.com/oauth/authorize?client_id=49974&response_type=code&redirect_uri=http://alisaalena.ddns.net/strava/exchange_token&approval_prompt=force&scope=read");
+            return new RedirectResult($"http://www.strava.com/oauth/authorize?client_id=49974&response_type=code&redirect_uri=http://alisaalena.ddns.net/strava/exchange_token&approval_prompt=force&scope=read,activity:read");
         }
-        public IActionResult exchange_token(string state, string code, string scope)
+        private static readonly HttpClient client = new HttpClient();
+        public async Task<IActionResult> exchange_token(string state, string code, string scope)
         {
             ViewData.Model = $"state {state},  code {code},  scope {scope}";
+            WebRequest request = WebRequest.Create("https://www.strava.com/oauth/token");
+            request.Method = "Post";
+            var values = new Dictionary<string, string>
+            {
+                { "client_id", "49974" },
+                { "client_secret", "1c65ed360b40e351d59a9f4e5081fbe7b4bb87cf" },
+                { "code", code },
+                {"grant_type","authorization_code" }
+            };
+            var content = new FormUrlEncodedContent(values);
+            try
+            {
+               
+                var response = await client.PostAsync("https://www.strava.com/oauth/token", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                ViewData.Model = responseString;
+            }
+            catch (Exception ex)
+            {
+                ViewData.Model = ex.Message;
+            }
+            //// Display the status.
+           
             return View();
+        }
+        public async Task<string> GetDataFromBody(HttpResponse Response)
+        {
+            string data;
+            using (StreamReader reader = new StreamReader(Response.Body))
+            {
+                data = await reader.ReadToEndAsync();
+            }
+            return data;
         }
         /// <summary>
         /// Strava login callback. 
