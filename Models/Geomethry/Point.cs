a2727a23ea82cd8fceb703a36a53f8625aaf6788
202547 <202547@mail.ru>
2020-06-20 16:03:22 +0300

    add geomethr_schema add dictionary_schema add user_schema

---
+++ Models/Geomethry/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniAppHakaton.Models.Geomethry
+{
+    public class Point
+    {
+        public long Id { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public ICollection<TracksPoints> TracksPoints { get; set; }
+    }
+}
---
+++ Models/Geomethry/Track.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniAppHakaton.Models.Geomethry
+{
+    public class Track
+    {
+        public long Id { get; set; }
+        public long StravaTrackId { get; set; }
+        public ICollection<TracksPoints> TracksPoints { get; set; }
+        public ICollection<Users.UserTrack> UserTracks { get; set; }
+    }
+}
